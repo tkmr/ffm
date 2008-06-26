1fa7d1b383f688408ddca086dc750aab71ce743d
tkmr <tkmr2000@gmail.com>
2008-06-27 00:57:31 +0900

    move css file

--- java/spec/TCPSocket_spec.rb
+++ java/spec/TCPSocket_spec.rb
@@ -7,11 +7,33 @@ describe FFM::Socket::TCPSocket do
     @socket = FFM::Socket::TCPSocket.new(80)
   end
 
-  it "can get a web page used http over tcp" do
+  it "can get a web page, and it have readAll() method." do
     @socket.connect("www.google.com", 5000) #time out 5 seconds.
     @socket.write("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
-    result = @socket.read();
-    @socket.close();
+    result = @socket.readAll()
+    @socket.close()
     result.should match /<html.*<\/html>/i
+  end
+
+  it "have read() method and get one byte" do
+    @socket.connect("www.google.com", 5000) #time out 5 seconds.
+    @socket.write("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
+    result = ""
+    while((tmp_res = @socket.read()) != -1)
+      result += tmp_res.to_s
+    end
+    @socket.close()
+    result.size.should >= 100
+  end
+
+  it "have read() method and get some bytes" do
+    @socket.connect("www.google.com", 5000) #time out 5 seconds.
+    @socket.write("GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n")
+    result = ""
+    while((tmp_res = @socket.read()) != -1)
+      result += tmp_res.to_s
+    end
+    @socket.close()
+    result.size > 100
   end
 end
